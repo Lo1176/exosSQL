49babded4a59a0f17eb13cbf037b2ea00a199aa3
Laurent Binder <laurentbinder@gmail.com>
2022-12-06 10:40:38 +0100

    add SQL1.1-02

--- SQL1.1-02-Requêtes-SELECT-simples.sql
+++ SQL1.1-02-Requêtes-SELECT-simples.sql
@@ -1,5 +1,5 @@
 /* La liste des bureaux (adresse et ville) triés par pays décroissant puis par état */
 /* SELECT addressLine1, city FROM `offices` ORDER BY "country","state"; */
-SELECT "addressLine1", "city", FROM `offices` ORDER BY "country" DESC ,"state" DESC;
+SELECT addressLine1, city FROM `offices` ORDER BY "country" DESC ,"state" DESC;
 
 /* RESULTAT ==> 7 lignes / 100 Market Street */
